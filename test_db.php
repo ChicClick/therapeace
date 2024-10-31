cc17bbd848427683a5f87caedb7493aec40e1c4c
ChicClick <chicclick05@gmail.com>
2024-10-31 23:36:52 +0800

    Create test_db.php

---
+++ test_db.php
@@ -0,0 +1,15 @@
+<?php
+$db_host = getenv('DB_HOST');
+$db_user = getenv('DB_USER');
+$db_password = getenv('DB_PASSWORD');
+$db_name = getenv('DB_NAME');
+
+// Attempt to connect to the database
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connected successfully to the database!";
+}
+?>
