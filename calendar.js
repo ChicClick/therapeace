a0430504baa7b7c632bc33dee21420de4236e72d
ChicClick <ChicClick@gmail.com>
2024-11-09 03:31:48 +0800

    FIX: fetch booking data error

--- calendar.js
+++ calendar.js
@@ -95,7 +95,7 @@ async function generateCalendar(selectedDay = null) {
 // Function to fetch booked dates for a therapist
 async function fetchBookedDatesForTherapist(therapistID) {
     try {
-        const response = await fetch(`/booked-dates.php?therapistID=${therapistID}`);
+        const response = await fetch(`booked-dates.php?therapistID=${therapistID}`);
         console.log(response);
         const data = await response.json();
         return data.bookedDates;
