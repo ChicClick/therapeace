d1963d5617357f4e6a7576dad680ab04f1fc5aa9
ChicClick <chicclick05@gmail.com>
2024-11-09 00:49:49 +0800

    Update patient_forgot_password.php

--- patient_forgot_password.php
+++ patient_forgot_password.php
@@ -67,7 +67,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                         // Content
                         $mail->isHTML(true);
                         $mail->Subject = 'Password Reset Request';
-                        $mail->Body = "Click the link below to reset your password:<br><a href='" . $resetLink . "'>Reset Password</a>";
+                        $mail->Body = "A request has been received to change the password for your TheraPeace account.<br><br>"
+                        - "Click the link below to reset your password<br><a href='" . $resetLink . "'>Reset Password</a>";
 
                         // Send the email
                         $mail->send();
