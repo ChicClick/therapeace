1a333f3a98c665b5bb3b6ea413dcfe7bda153fcb
ChicClick <chicclick05@gmail.com>
2024-11-08 16:15:23 +0800

    Update patient_forgot_password.php

--- patient_forgot_password.php
+++ patient_forgot_password.php
@@ -1,9 +1,9 @@
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 $mail = new PHPMailer(true);
 
