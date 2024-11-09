938ce784048668da20d98a38d1e5c6cc04891220
ChicClick <chicclick05@gmail.com>
2024-11-09 11:10:29 +0800

    Update add_notes.php

--- add_notes.php
+++ add_notes.php
@@ -4,6 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+include 'config.php';
 include 'db_conn.php'; // Include your database connection file
 
 session_start(); // Start the session
