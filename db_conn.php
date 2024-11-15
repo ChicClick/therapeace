075675b2220c3fa4be6e4182990b4a58170c3983
SvenSven <mikorecare@gmail.com>
2024-11-15 17:00:21 +0800

    CHORE: Revert DB local settings

--- db_conn.php
+++ db_conn.php
@@ -5,7 +5,7 @@ $db_user = getenv('DB_USER');
 $db_password = getenv('DB_PASSWORD');
 $db_name = getenv('DB_NAME');
 
-$conn = new mysqli("localhost", "root", "", "therapeacedb", 8111);
+$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
