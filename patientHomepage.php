c503bbf732def52b8ec48ecb97cb6717e5c5fcb3
ChicClick <ChicClick@gmail.com>
2025-02-04 13:03:41 +0800

    STYLE: homepage font

--- patientHomepage.php
+++ patientHomepage.php
@@ -22,7 +22,7 @@ if (isset($_SESSION['patientName'])) {
 
     <link href="https://fonts.googleapis.com/css2?family=Architects+Daughter&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poly:ital@0;1&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;800&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 
